ed7d51dc15e8f0f45109700826052cf152c964bf
Gabriel Cior <gabriel.cior@gmail.com>
2021-01-29 12:02:01 +0100

    Ajouter

--- pers.h
+++ pers.h
@@ -1,4 +1,5 @@
 #include <utility>
+#include <cmath>
 using namespace std;
 
 class person{
@@ -13,26 +14,31 @@ class person{
         char getchar(){
             return e;
         }
+        bool isalive(){
+            return alive;
+        }
     private :
         std::pair <int,int> coord;
         char e; // char de représentation
-        int str; //point de force
     protected:
         person(int x0,int y0,int h0,char c0,int str0):coord(x0,y0),h(h0),str(str0){}; //Constructeur private à accéder grace aux sous-classe !PAS DE PERSON H DANS LE CODE!
         int h; //points de vie
+        bool alive= true;
+        int str; //point de force
 };
 
 class enemy:public person{
     void hit(int d,Hero* H){
         if (h-d>0){
-            h-=d;
+            h-=d; //réduit sa vie
         }
         else{
             die(H);
+            alive=false;//meurt si il a plus de vie
         }
     }
     void die(Hero* H){
-        H->plusexp(expgiven)
+        H->plusexp(expgiven);
     }
     private:
         int expgiven; //expérience donnée au joueur en mourant
@@ -72,14 +78,28 @@ class bme:public enemy{
 
 class Hero:public person{
     public :
-        Hero(int x0,int y0):person(x0,y0,10,'@',2),mana(10),exp(0),level(1),intel(3){
+        Hero(int x0,int y0):person(x0,y0,10,'@',2),mana(10),exp(0),level(1),intel(3),armor(1){
         };
         void plusexp(int plus){
             exp+=plus; // rajoute l'exp au personnage
-            getlevel()
+            getlevel();
         }
         void getlevel(){
-
+            while (exp < pow(level,2) ){
+                level +=1;
+                h+=5;
+                hmax+=5;
+                str+=2;
+                intel+=2;
+            }
+        }
+        void hit(int d){
+            if (d<h){
+                h-=d;
+            }
+            else{
+                alive=false;
+            }
         }
     private:
         int mana; //point de magie nécéssaire pour prendre des dégats
@@ -87,4 +107,6 @@ class Hero:public person{
         int intel; //point d'intelligence, comme les points de force mais pour la magie
         int gold; // argent
         int exp; //expérience du personnage(à augmenter pour gagner en niveau)  
+        int armor; // armure du personnage
+        int hmax=10;
 };
