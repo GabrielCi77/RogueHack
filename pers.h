c6de402fd38839d8c7ecc7f520b5876adf7b1177
Gabriel Cior <gabriel.cior@gmail.com>
2021-01-29 11:18:50 +0100

    Amelioration

--- pers.h
+++ pers.h
@@ -1,73 +1,90 @@
+#include <utility>
+using namespace std;
+
 class person{
     public :
         void move (int x0,int y0){
-            x+=x0;
-            y+=y0;
+            coord.second+=x0;
+            coord.first+=y0;
         } // Change la position, à utiliser surement avec des + ou -1 seulement pour éviter des téléportations
-        void hit(int d){
-            if (h-d>0){
-                h-=d; //baisse les points de vie
-            }
-            else{
-                die(); // meurt
-            }
+        pair<int,int> getpos(){
+            return coord;
         }
-        void attack(person* H){
-            H->hit(str); //la personne attaquée prend des dégats
+        char getchar(){
+            return e;
         }
-        void die(){
-
-        };
     private :
-        int x;
-        int y;
-        int h; //points de vie
+        std::pair <int,int> coord;
         char e; // char de représentation
         int str; //point de force
     protected:
-        person(int x0,int y0,int h0,char c0,int str0):x(x0),y(y0),h(h0),str(str0){}; //Constructeur private à accéder grace aux sous-classe !PAS DE PERSON H DANS LE CODE!
+        person(int x0,int y0,int h0,char c0,int str0):coord(x0,y0),h(h0),str(str0){}; //Constructeur private à accéder grace aux sous-classe !PAS DE PERSON H DANS LE CODE!
+        int h; //points de vie
 };
 
-class bat:public person{
+class enemy:public person{
+    void hit(int d,Hero* H){
+        if (h-d>0){
+            h-=d;
+        }
+        else{
+            die(H);
+        }
+    }
+    void die(Hero* H){
+        H->plusexp(expgiven)
+    }
+    private:
+        int expgiven; //expérience donnée au joueur en mourant
+    protected:
+        enemy(int x0,int y0,int h0,char c0,int str0,int expgiven0):person(x0,y0,h0,c0,str0),expgiven(expgiven0){};
+};
+
+class bat:public enemy{
     public :
-        bat(int x0,int y0):person(x0,y0,3,'~',1){
+        bat(int x0,int y0):enemy(x0,y0,3,'~',1,20){
         };
 };
 
-class DK:public person{
+class DK:public enemy{
     public :
-        DK(int x0,int y0):person(x0,y0,30,'#',5){
+        DK(int x0,int y0):enemy(x0,y0,30,'£',5,1000){
         };
 };
 
-class goblin:public person{
+class goblin:public enemy{
     public :
-        goblin(int x0,int y0):person(x0,y0,6,'ù',2){
+        goblin(int x0,int y0):enemy(x0,y0,6,'ù',2,50){
         };
 };
 
-class skeleton:public person{
+class skeleton:public enemy{
     public :
-        skeleton(int x0,int y0):person(x0,y0,8,'$',3){
+        skeleton(int x0,int y0):enemy(x0,y0,8,'$',3,100){
         };
 };
 
-class bme:public person{
+class bme:public enemy{
     public :
-        bme(int x0,int y0):person(x0,y0,2,'ç',1){
+        bme(int x0,int y0):enemy(x0,y0,2,'ç',1,10){
         };
 };
 
 class Hero:public person{
-
     public :
         Hero(int x0,int y0):person(x0,y0,10,'@',2),mana(10),exp(0),level(1),intel(3){
         };
-        int exp; //expérience du personnage(à augmenter pour gagner en niveau)
+        void plusexp(int plus){
+            exp+=plus; // rajoute l'exp au personnage
+            getlevel()
+        }
+        void getlevel(){
 
+        }
     private:
         int mana; //point de magie nécéssaire pour prendre des dégats
         int level; // le niveau du personnage
         int intel; //point d'intelligence, comme les points de force mais pour la magie
-    
+        int gold; // argent
+        int exp; //expérience du personnage(à augmenter pour gagner en niveau)  
 };
